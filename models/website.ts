907603073cba00417146aad96fdc129f8ae41cde
rodyherrera <contact@rodyherrera.com>
2024-05-23 21:54:08 -0400

    feat: search for keywords in metadata of sites & services to improve search engines.

--- models/website.ts
+++ models/website.ts
@@ -32,6 +32,30 @@ const websiteSchema = new mongoose.Schema<WebsiteDocument>({
 websiteSchema.index({ url: 1 });
 websiteSchema.index({ url: 'text', title: 'text', description: 'text' });
 
+interface UniqueKeywords{
+    createdAt: Date,
+    keyword: string
+};
+
+export const getUniqueKeywords = async (aggregateOpts: object[]): Promise<UniqueKeywords[]> => {
+    const uniqueKeywords = await Website.aggregate([
+        // Filter documents where metaData.keywords is defined and not null
+        { $match: { 'metaData.keywords': { $exists: true, $ne: null } } },
+        // Split the metaData.keywords string into an array of keywords and keep the createdAt field
+        { $project: { keywords: { $split: ['$metaData.keywords', ','] }, createdAt: 1 } },
+        // Convert each keyword into a separate document
+        { $unwind: '$keywords' },
+         // Convert keywords to lowercase, apply trim, and keep the createdAt field
+        { $project: { keyword: { $trim: { input: { $toLower: '$keywords' } } }, createdAt: 1 } },
+        // Group by keywords and keep the first createdAt value
+        { $group: { _id: '$keyword', createdAt: { $first: '$createdAt' } } },
+        // Rename field "_id" to "keyword"
+        { $project: { keyword: '$_id', createdAt: 1, _id: 0 } },
+        ...(aggregateOpts as any[])
+    ]);
+    return uniqueKeywords;
+};
+
 const Website: Model<WebsiteDocument> = mongoose.model<WebsiteDocument>('Website', websiteSchema);
 
 export default Website;
---
+++ services/suggestEngineImprovement.ts
@@ -0,0 +1,36 @@
+import { EventEmitter } from 'events';
+import { getUniqueKeywords } from '@models/website';
+import Suggest from '@models/suggest';
+
+// Each improvement method will emit events related to the lifecycle 
+// of its operation. Each event sends an object with the 'method'
+// parameter, which indicates which improvisation method the 
+// emitted event corresponds to.
+class SuggestEngineImprovement extends EventEmitter{
+    async websitesKeywordBasedImprovement(batchSize: number = 1000): Promise<any>{
+        const method = 'websitesKeywordBased';
+        this.emit('improvementStart', { method });
+        let skip = 0;
+        // An iteration will be carried out until all records are processed.
+        while(true){
+            // We use the latest records of indexed websites, because in this 
+            // way we obtain new keywords as the site search engine also receives 
+            // feedback. Otherwise, we would always obtain data that 
+            // is already in the database.
+            const uniqueKeywords = await getUniqueKeywords([
+                { $sort: { createdAt: -1 } },
+                { $limit: batchSize }
+            ]);
+            if(uniqueKeywords.length === 0) break;
+            const bulksOps = uniqueKeywords.map(({ keyword }) => ({
+                insertOne: { document: { suggest: keyword } }
+            }));
+            await Suggest.bulkWrite(bulksOps, { ordered: false });
+            this.emit('batchProcessed', { data: uniqueKeywords });
+            skip += batchSize;
+        }
+        this.emit('improvementEnd', { method });
+    };
+};
+
+export default SuggestEngineImprovement;
