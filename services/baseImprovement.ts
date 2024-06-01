a67dc5656a231594e7a0bf180626976ce7e7b75c
rodyherrera <contact@rodyherrera.com>
2024-06-01 00:08:55 -0400

    docs: @services/baseImprovement.ts

--- services/baseImprovement.ts
+++ services/baseImprovement.ts
@@ -1,7 +1,17 @@
 import { EventEmitter } from 'events';
 import Website from '@models/website';
 
+/**
+ * A base class for improvements.
+*/
 class BaseImprovement extends EventEmitter{
+    /**
+     * Processes an improvement.
+     * @param {string} method - The method to use for the improvement.
+     * @param {number} batchSize - The batch size for processing.
+     * @param {function} getDataFunc - A function to get the data for processing.
+     * @returns {Promise<void>} A promise that resolves when the improvement is complete.
+    */
     async processImprovement(
         method: string,
         batchSize: number,
@@ -24,7 +34,13 @@ class BaseImprovement extends EventEmitter{
         this.emit('improvementEnd');
     };
 
-    // Shared function in suggestEngineImprovement.ts & websiteEngineImprovement.ts
+    /**
+     * Gets websites from the database.
+     * @param {number} skip - The number of documents to skip.
+     * @param {number} limit - The number of documents to limit.
+     * @param {number} createdAt - The sort order for the createdAt field.
+     * @returns {Promise<{ url: string }[]>} A promise that resolves to an array of websites.
+    */
     async getWebsitesFromDatabase(skip: number, limit: number, createdAt: -1 | 1): Promise<{ url: string }[]>{
         return await Website.aggregate([
             { $sort: { createdAt } },
@@ -34,10 +50,19 @@ class BaseImprovement extends EventEmitter{
         ]);
     };
 
+    /**
+     * Gets the bulk operations for an item.
+     * @param {any} item - The item to get the bulk operations for.
+     * @returns {any} The bulk operations.
+    */
     getBulkOps(item: any){
         throw new Error('@services/baseImprovement.ts - getBulkOps - not implemented.');
     };
 
+    /**
+     * Performs a bulk write operation.
+     * @param {any[]} bulkOps - The bulk operations to perform.
+    */
     performBulkWrite(bulkOps: any[]): void{
         throw new Error('@services/baseImprovement.ts - performBulkWrite - not implemented.');
     };
