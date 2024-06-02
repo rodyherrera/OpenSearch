998bdd8d4f3696c9363227bfdfe9ab21f8c21479
rodyherrera <contact@rodyherrera.com>
2024-06-02 00:08:07 -0400

    docs: @services/suggestEngineImprovement.ts

--- services/suggestEngineImprovement.ts
+++ services/suggestEngineImprovement.ts
@@ -5,16 +5,28 @@ import PQueue from 'p-queue';
 import { getUniqueKeywords } from '@utilities/websites';
 import { suggestionsFromContent } from '@utilities/suggestions';
 
+/**
+ * Class for improving suggestions search engine.
+ * @extends BaseImprovement
+*/
 class SuggestEngineImprovement extends BaseImprovement{
     private webScraper: WebScraper;
     private suggestQueue: PQueue;
 
+    /**
+     * Creates an instance of SuggestEngineImprovement.
+    */
     constructor(){
         super();
         this.webScraper = new WebScraper();
         this.suggestQueue = new PQueue({ concurrency: 5 });
     };
 
+    /**
+     * Performs content-based improvement of suggestions.
+     * @param {number} [batchSize=5] - The number of websites to process in each batch.
+     * @returns {Promise<void>} - A promise that resolves when improvement is complete.
+    */
     async contentBasedImprovement(batchSize: number = 5): Promise<void>{
         const method = 'contentBased';
         const getDataFunc = (createdAt: 1 | -1) => async (skip: number) => {
@@ -29,6 +41,11 @@ class SuggestEngineImprovement extends BaseImprovement{
         ]);
     };
 
+    /**
+     * Performs keyword-based improvement of suggestions.
+     * @param {number} [batchSize=1] - The number of suggestions to process in each batch.
+     * @returns {Promise<void>} - A promise that resolves when improvement is complete.
+    */
     async keywordBasedImprovement(batchSize: number = 1): Promise<void>{
         const method = 'keywordBased';
         const getDataFunc = (createdAt: number) => async (skip: number) => {
@@ -45,10 +62,20 @@ class SuggestEngineImprovement extends BaseImprovement{
         ]);
     };
 
+    /**
+     * Performs bulk write operations for suggestions.
+     * @param {any[]} bulkOps - The array of bulk write operations.
+     * @returns {Promise<void>} - A promise that resolves when bulk write is complete.
+    */
     async performBulkWrite(bulkOps: any[]): Promise<void>{
         await Suggest.bulkWrite(bulkOps, { ordered: false });
     };
 
+    /**
+     * Generates bulk write operations for suggestions.
+     * @param {any} item - The suggestion data.
+     * @returns {any} - The bulk write operation document.
+    */
     getBulkOps(item: any): any{
         return {
             updateOne: {
