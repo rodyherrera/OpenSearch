d9f35a017b308a124cbf894a296e2048f950199a
rodyherrera <contact@rodyherrera.com>
2024-06-02 00:10:03 -0400

    docs: @services/websiteEngineImprovement.ts

--- services/websiteEngineImprovement.ts
+++ services/websiteEngineImprovement.ts
@@ -6,20 +6,39 @@ import WebScraper from '@services/webScraper';
 // @ts-ignore
 import CDrakeSE from 'cdrake-se';
 
+/**
+ * Class for improving websites search engine.
+ * @extends BaseImprovement
+*/
 class WebsiteEngineImprovement extends BaseImprovement{
     private webScraper: WebScraper;
 
+    /**
+     * Creates an instance of WebsiteEngineImprovement.
+    */
     constructor(){
         super();
         this.webScraper = new WebScraper();
     };
 
+    /**
+     * Processes a suggest string to extract links and data.
+     * @param {string} suggest - The suggest string.
+     * @returns {Promise<BulkWriteDocument[]>} - A promise resolving to an array of bulk write operations.
+     * @private
+    */
     private async processSuggest(suggest: string): Promise<BulkWriteDocument[]>{
         const links = await this.getLinksFromSuggest(suggest);
         const scrapedWebsites = await this.scrapeLinksAndExtractData(links);
         return scrapedWebsites.map(this.getBulkOps);
     };
 
+    /**
+     * Extracts links from a suggest string using CDrakeSE.
+     * @param {string} suggest - The suggest string.
+     * @returns {Promise<string[]>} - A promise resolving to an array of links.
+     * @private
+    */
     private async getLinksFromSuggest(suggest: string): Promise<string[]>{
         try{
             const { Results: webSearch } = await CDrakeSE({ Query: suggest, Method: 'Search' });
@@ -29,12 +48,23 @@ class WebsiteEngineImprovement extends BaseImprovement{
         }
     };
 
+    /**
+     * Scrapes links and extracts data from websites.
+     * @param {string[]} links - The array of website links.
+     * @returns {Promise<BulkWriteDocument[]>} - A promise resolving to an array of bulk write operations.
+     * @private
+    */
     private async scrapeLinksAndExtractData(links: string[]): Promise<BulkWriteDocument[]>{
         const promises = links.map((link) => this.webScraper.scrapeSite(link));
         const results = await Promise.all(promises.map((p) => p.catch((error) => error)));
         return results.filter((result) => result !== null);
     };
 
+    /**
+     * Performs hyperlink-based improvement of websites.
+     * @param {number} [batchSize=1] - The number of websites to process in each batch.
+     * @returns {Promise<void>} - A promise that resolves when improvement is complete.
+    */
     async hyperlinkBasedImprovement(batchSize: number = 1): Promise<void>{
         const method = 'hyperlinkBased';
         const getDataFunc = (createdAt: -1 | 1) => async (skip: number) => {
@@ -48,6 +78,11 @@ class WebsiteEngineImprovement extends BaseImprovement{
         ]);
     };
 
+    /**
+     * Performs suggest-based improvement of websites.
+     * @param {number} [batchSize=1] - The number of suggestions to process in each batch.
+     * @returns {Promise<void>} - A promise that resolves when improvement is complete.
+    */
     async suggestsBasedImprovement(batchSize: number = 1): Promise<void>{
         const method = 'suggestsBased';
         const getDataFunc = (createdAt: -1 | 1) => async (skip: number) => {
@@ -66,6 +101,11 @@ class WebsiteEngineImprovement extends BaseImprovement{
         ]);
     };
 
+    /**
+     * Generates bulk write operations for websites.
+     * @param {any} item - The website data.
+     * @returns {BulkWriteDocument} - The bulk write operation document.
+    */
     getBulkOps(item: any): BulkWriteDocument{
         const { url, title, description, metaData } = item;
         return {
@@ -77,11 +117,20 @@ class WebsiteEngineImprovement extends BaseImprovement{
         };
     };
 
+    /**
+     * Performs bulk write operations for websites.
+     * @param {BulkWriteDocument[]} bulkOps - The array of bulk write operations.
+     * @returns {void}
+    */
     performBulkWrite(bulkOps: any[]): void{
         Website.bulkWrite(bulkOps, { ordered: false });
     };
 };
 
+/**
+ * Interface representing a bulk write operation document for websites.
+ * @interface
+*/
 interface BulkWriteDocument {
     updateOne: {
         filter: { url: string };
