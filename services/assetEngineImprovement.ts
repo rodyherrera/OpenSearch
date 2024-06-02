147af3ca49c33c3258088ee91e4caa13534d4976
rodyherrera <contact@rodyherrera.com>
2024-06-02 00:07:48 -0400

    docs: @services/assetEngineImprovement.ts

--- services/assetEngineImprovement.ts
+++ services/assetEngineImprovement.ts
@@ -3,14 +3,26 @@ import BaseImprovement from '@services/baseImprovement';
 import WebScraper from '@services/webScraper';
 import { ScrapedAsset } from '@services/htmlDataExtractor';
 
+/**
+ * Class for improving assets search engine.
+ * @extends BaseImprovement
+*/
 class AssetEngineImprovement extends BaseImprovement{
     private assetScraper: WebScraper;
 
+    /**
+     * Creates an instance of AssetEngineImprovement.
+    */
     constructor(){
         super();
         this.assetScraper = new WebScraper();
     };
 
+    /**
+     * Performs content-based improvement of assets.
+     * @param {number} [batchSize=1] - The number of websites to process in each batch.
+     * @returns {Promise<void>} - A promise that resolves when improvement is complete.
+    */
     async contentBasedImprovement(batchSize: number = 1): Promise<void>{
         const method = 'contentBased';
         const getDataFunc = (createdAt: -1 | 1) => async (skip: number) => {
@@ -24,6 +36,11 @@ class AssetEngineImprovement extends BaseImprovement{
         ]);
     };
 
+    /**
+     * Generates bulk write operations for assets.
+     * @param {ScrapedAsset} item - The asset data.
+     * @returns {BulkWriteDocument} - The bulk write operation document.
+    */
     getBulkOps(item: ScrapedAsset): BulkWriteDocument{
         const { url, parentUrl, type } = item;
         return {
@@ -35,11 +52,20 @@ class AssetEngineImprovement extends BaseImprovement{
         };
     };
 
+    /**
+     * Performs bulk write operations for assets.
+     * @param {any[]} bulkOps - The array of bulk write operations.
+     * @returns {void}
+    */
     performBulkWrite(bulkOps: any[]): void{
         Asset.bulkWrite(bulkOps, { ordered: false });
     }
 };
 
+/**
+ * Interface representing a bulk write operation document.
+ * @interface
+*/
 interface BulkWriteDocument{
     updateOne: {
         filter: { url: string },
