1188d64dec96833df141a99ed9e7b4f3f68c8692
rodyherrera <contact@rodyherrera.com>
2024-06-02 00:04:01 -0400

    docs: @services/imageEngineImprovement.ts

--- services/imageEngineImprovement.ts
+++ services/imageEngineImprovement.ts
@@ -3,14 +3,26 @@ import BaseImprovement from '@services/baseImprovement';
 import WebScraper from '@services/webScraper';
 import { ScrapedImage } from '@services/htmlDataExtractor';
 
+/**
+ * Class for improving images search engine.
+ * @extends BaseImprovement
+*/
 class ImageEngineImprovement extends BaseImprovement{
     private imageScraper: WebScraper;
-
+    
+    /**
+     * Creates an instance of ImageEngineImprovement.
+    */
     constructor(){
         super();
         this.imageScraper = new WebScraper();
     };
 
+    /**
+     * Performs content-based improvement of images.
+     * @param {number} [batchSize=1] - The number of websites to process in each batch.
+     * @returns {Promise<void>} - A promise that resolves when improvement is complete.
+    */
     async contentBasedImprovement(batchSize: number = 1): Promise<void>{
         const method = 'contentBased';
         const getDataFunc = (createdAt: -1 | 1) => async (skip: number) => {
@@ -24,6 +36,11 @@ class ImageEngineImprovement extends BaseImprovement{
         ]);
     };
 
+    /**
+     * Generates bulk write operations for images.
+     * @param {ScrapedImage} item - The image data.
+     * @returns {BulkWriteDocument} - The bulk write operation document.
+    */
     getBulkOps(item: ScrapedImage): BulkWriteDocument{
         const { src, width, height, alt } = item;
         return {
@@ -35,11 +52,20 @@ class ImageEngineImprovement extends BaseImprovement{
         };
     };
 
+    /**
+     * Performs bulk write operations for images.
+     * @param {any[]} bulkOps - The array of bulk write operations.
+     * @returns {void}
+    */
     performBulkWrite(bulkOps: any[]): void{
         Image.bulkWrite(bulkOps, { ordered: false });
     };
 };
 
+/**
+ * Interface representing a bulk write operation document.
+ * @interface
+*/
 interface BulkWriteDocument{
     updateOne: {
         filter: { src: string },
